c9821f7f0b2efec96dec92b900899352fe2b5ca4
hasanraiyan <143262732+hasanraiyan@users.noreply.github.com>
2023-11-11 19:27:00 +0530

    Create question1ToPrintMutiply.java

---
+++ Questions/question1ToPrintMutiply.java
@@ -0,0 +1,22 @@
+// Question: Print table from 2 to n ?
+import java.util.Scanner;
+
+public class Main{
+    static void multiply(int n){
+        for (int i=1 ; i<11; i++){
+
+            int z= n*i;
+            System.out.println(n + " * " + i + " = " + z);
+        }
+    }
+    public static void main(String[] args){
+        int z;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number till you want to get mulptiple of: ");
+        z = scan.nextInt();
+        for (int i =2 ; i<z ;i++){
+            System.out.println("Table of "+i);
+            multiply(i);
+        }
+    }
+}
