4999f24b308e9cb379b95d1aa319dabeb6056e3b
hasanraiyan <143262732+hasanraiyan@users.noreply.github.com>
2023-11-11 21:23:50 +0530

    Create javaLec03.java
    Certainly! Here's a description of the code:
    
    This Java code is a simple program that demonstrates the declaration and initialization of variables of various data types and then prints their values to the console. Let's break down the code step by step:
    
    1. `int x = 5;`: This line declares and initializes an integer variable named `x` with the value 5.
    
    2. `double y = 10.5;`: This line declares and initializes a double variable named `y` with the value 10.5.
    
    3. `char c = 'c';`: This line declares and initializes a character variable named `c` with the character 'c'.
    
    4. `boolean b = true;`: This line declares and initializes a boolean variable named `b` with the value `true`.
    
    5. `String s = "Hello World";`: This line declares and initializes a string variable named `s` with the text "Hello World".
    
    6. `System.out.println(x);`: This line prints the value of `x` (which is 5) to the console.
    
    7. `System.out.println(y);`: This line prints the value of `y` (which is 10.5) to the console.
    
    8. `System.out.println(c);`: This line prints the value of `c` (which is the character 'c') to the console.
    
    9. `System.out.println(b);`: This line prints the value of `b` (which is `true`) to the console.
    
    10. `System.out.println(s);`: This line prints the value of `s` (which is "Hello World") to the console.
    
    In summary, this code showcases the declaration and initialization of variables of different data types (int, double, char, boolean, and String) and demonstrates how to print their values to the console using `System.out.println()`. It's a basic example to illustrate variable usage in Java.

---
+++ Tutorial/javaLec03.java
@@ -0,0 +1,47 @@
+// java variables- declaration and use
+    public class Main {
+      public static void main(String[] args) {
+        // Declare and initialize integer 'x' with the value 5.
+        int x = 5;
+
+        // Declare and initialize double 'y' with the value 10.5.
+        double y = 10.5;
+
+        // Declare and initialize character 'c' with the character 'c'.
+        char c = 'c';
+
+        // Declare and initialize boolean 'b' with the value 'true'.
+        boolean b = true;
+
+        // Declare and initialize a string 's' with the text "Hello World".
+        String s = "Hello World";
+
+        // Print the value of 'x' to the console.
+        System.out.println(x);
+
+        // Print the value of 'y' to the console.
+        System.out.println(y);
+
+        // Print the value of 'c' to the console.
+        System.out.println(c);
+
+        // Print the value of 'b' to the console.
+        System.out.println(b);
+
+        // Print the value of 's' to the console.
+        System.out.println(s);
+
+    // use print to print naming rue of varibles in java 
+    System.out.println("Naming rule in Java:");
+    System.out.println("1. Variable names should start with a letter or an underscore.");
+    System.out.println("2. Variable names should not start with a number.");
+
+    System.out.println("3. Variable names should be unique.");
+    System.out.println("4. Variable names should not contain spaces or special character.");
+    System.out.println("5. Variable names should not be keywords.");
+    System.out.println("6. Variable names should be descriptive.");
+    System.out.println("7. Variable names should be short.");
+    System.out.println("8. Variable names should be camelCase.");
+
+  }
+}
