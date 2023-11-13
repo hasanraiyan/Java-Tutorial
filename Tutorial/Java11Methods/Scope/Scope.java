432fc8fb50c30873b97877271f324acd701b1f8a
hasanraiyan <143262732+hasanraiyan@users.noreply.github.com>
2023-11-13 11:30:13 +0530

    Update Scope.java

--- Tutorial/Java11Methods/Scope/Scope.java
+++ Tutorial/Java11Methods/Scope/Scope.java
@@ -1,1 +1,7 @@
 //Java Scope: In java , variables are only accessible within the block they are defined.
+public class Scope {
+  public static void main(String[] args) {
+    int x = 10;
+    System.out.println("x = " + x);
+  }
+}
