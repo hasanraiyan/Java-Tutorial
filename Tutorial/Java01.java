fab0e021b760dd41293ac15539baa243c9a7791e
hasanraiyan <143262732+hasanraiyan@users.noreply.github.com>
2023-11-22 09:00:22 +0530

    Delete Tutorial directory

--- Tutorial/Java01.java
+++
@@ -1,8 +0,0 @@
-// basic code  to print Hello Java
-// important note java is case sensitive so "Myclass" and "myclass" are different
-
-public class Java01{ // This line defines a class named 'Java01'
-  public static void main(String[] args) { // This is the main method, the entry point of the program.
-    System.out.println("Hello, Java!"); // This line prints the text "Hello, Java!" to the console.
-  }
-}
--- Tutorial/Java11Methods/Scope/Scope.java
+++
@@ -1,7 +0,0 @@
-//Java Scope: In java , variables are only accessible within the block they are defined.
-public class Scope {
-  public static void main(String[] args) {
-    int x = 10;
-    System.out.println("x = " + x);
-  }
-}
